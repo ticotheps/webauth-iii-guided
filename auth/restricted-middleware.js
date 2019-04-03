30631c531ade4f8949fb9ccba9a2c2e58422b70b
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-03 12:05:38 -0400

    Simplifies the 'restricted()' middleware function due to the addition of 'jsonwebtoken'

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -2,23 +2,21 @@ const bcrypt = require('bcryptjs');
 
 const Users = require('../users/users-model.js');
 
+// Step 5: Simplify the 'restricted()' middleware function due to
+// addition of 'jsonwebtoken'
 module.exports = (req, res, next) => {
-  const { username, password } = req.headers;
+  const token = req.headers.authorization;
 
-  if (username && password) {
-    Users.findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: 'Invalid Credentials' });
-        }
-      })
-      .catch(error => {
-        res.status(500).json({ message: 'Ran into an unexpected error' });
-      });
-  } else {
-    res.status(400).json({ message: 'No credentials provided' });
+  if (token) {
+    jwt.verify(token, secret, (err, decodedToken) => {
+      if (err) {
+        // record the event
+        res.status(401).json({ message: 'You shall not pass!' });
+      } else {
+        next();
+      }
+    });
+  } else {  
+    res.status(401).json({ message: 'You shall not pass!' });
   }
 };
