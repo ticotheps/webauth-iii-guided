8a3ce9988db94bcdd7a7cbb225d05425d10ff97b
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-03 13:59:45 -0400

    Adds 'req.decodedJwt &&' to the if/else statement of the withRole(role) middleware function inside the users-router.js file

--- users/users-router.js
+++ users/users-router.js
@@ -1,9 +1,9 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const Users = require('./users-model.js');
-const restricted = require('../auth/restricted-middleware.js');
+const Users = require("./users-model.js");
+const restricted = require("../auth/restricted-middleware.js");
 
-router.get('/', restricted, withRole('student'), (req, res) => {
+router.get("/", restricted, withRole("student"), (req, res) => {
   Users.find()
     .then(users => {
       res.json(users);
@@ -13,13 +13,16 @@ router.get('/', restricted, withRole('student'), (req, res) => {
 
 function withRole(role) {
   return function(req, res, next) {
-    if(req.decodedJwt.roles && req.decodedJwt.roles.includes(role)) {
-
+    if (
+      req.decodedJwt &&
+      req.decodedJwt.roles &&
+      req.decodedJwt.roles.includes(role)
+    ) {
       next();
     } else {
-      res.status(403).json({ message: 'You have no power here!' });
+      res.status(403).json({ message: "You have no power here!" });
     }
-  }
-};
+  };
+}
 
 module.exports = router;
