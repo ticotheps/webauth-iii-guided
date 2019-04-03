5b52e0efad5718ca15d8c18442548ed1bd40fd4e
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-03 11:14:34 -0400

    Imports 'jsonwebtoken' into the server.js file

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
