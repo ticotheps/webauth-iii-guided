8c1ed6b45d28486f77ce6a2ba8bdfd2fe1b3b044
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-03 13:13:24 -0400

    Adds comments to the restricted-middleware.js file and imports 'jsonwebtoken' into restricted-middleware.js as well

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,13 +1,14 @@
 const bcrypt = require('bcryptjs');
-
+const jwt = require('jsonwebtoken'); // Step 6a: Import 'jsonwebtoken'
 const Users = require('../users/users-model.js');
 
-// Step 5: Simplify the 'restricted()' middleware function due to
+// Step 6b: Simplify the 'restricted()' middleware function due to
 // addition of 'jsonwebtoken'
 module.exports = (req, res, next) => {
   const token = req.headers.authorization;
 
   if (token) {
+    // is it valid?
     jwt.verify(token, secret, (err, decodedToken) => {
       if (err) {
         // record the event
