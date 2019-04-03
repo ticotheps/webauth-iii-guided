98b1dbaa1369916d5dcd30f17df2c01a3d8fa67f
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-03 13:55:41 -0400

    Creates a new 'withRole(role)' middleware that checks for a matching role from the decoded token before giving access to the database

--- users/users-router.js
+++ users/users-router.js
@@ -3,12 +3,23 @@ const router = require('express').Router();
 const Users = require('./users-model.js');
 const restricted = require('../auth/restricted-middleware.js');
 
-router.get('/', restricted, (req, res) => {
+router.get('/', restricted, withRole('student'), (req, res) => {
   Users.find()
     .then(users => {
       res.json(users);
     })
     .catch(err => res.send(err));
 });
+
+function withRole(role) {
+  return function(req, res, next) {
+    if(req.decodedJwt.roles && req.decodedJwt.roles.includes(role)) {
+
+      next();
+    } else {
+      res.status(403).json({ message: 'You have no power here!' });
+    }
+  }
+};
 
 module.exports = router;
